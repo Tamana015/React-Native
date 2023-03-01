2f2d57b50e6cee49a014c4a8b6ee9f3b73d864af
Tamana Thakur <rishabh.gupta@discounttire.com>
2023-03-01 14:37:12 +0530

    WelcomeScreen Fetch Message

--- AutenticationApp/screens/WelcomeScreen.js
+++ AutenticationApp/screens/WelcomeScreen.js
@@ -1,10 +1,26 @@
 import { StyleSheet, Text, View } from 'react-native';
+import axios from 'axios';
+import { useContext, useEffect, useState } from 'react';
+import { AuthenticationContex } from '../Store/AuthenticationContext';
 
 function WelcomeScreen() {
+  const [fetchMessage, SetMessage] = useState();
+  const authData = useContext(AuthenticationContex)
+  const token = authData.token
+  useEffect(() => {
+    async function getAuthData()
+    {
+      const response = await axios.get('https://adidos-4b7b5.firebaseio.com/message.json?auth=' +
+      token)
+      SetMessage(response.data)
+    }
+    getAuthData();
+  },[])
   return (
     <View style={styles.rootContainer}>
       <Text style={styles.title}>Welcome!</Text>
       <Text>You authenticated successfully!</Text>
+      <Text>{fetchMessage}</Text>
     </View>
   );
 }
